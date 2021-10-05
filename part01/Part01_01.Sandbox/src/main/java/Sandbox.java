adf5474ee1ab4b160d2f2b6f2dcaf942f709eda2
yavuzzs <vaohdesign@gmail.com>
2021-10-05 17:48:46 +0300

    sand box 1

--- part01/Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01/Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,6 +3,6 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
-        System.out.println("yavuz");
+        System.out.println("Sandbox");
     }
 }
