eb19025975d9a84591721558da437a421fabd5fb
yavuzzs <vaohdesign@gmail.com>
2021-10-05 18:22:07 +0300

    Dinoasaur

--- part01/Part01_05.Message/src/main/java/Message.java
+++ part01/Part01_05.Message/src/main/java/Message.java
@@ -1,13 +1,25 @@
 
+
+
 import java.util.Scanner;
 
 public class Message {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write a message:");
-        // Write your program here
+            // Create a tool for reading user input and name it scanner
+            Scanner scanner = new Scanner(System.in);
+
+            // Print "Write a message: "
+            System.out.println("Write a message: ");
+
+            // Read the string written by the user, and assign it
+            // to program memory "String message = (string that was given as input)"
+            String objects = scanner.nextLine();
+
+            // Print the message written by the user
+            System.out.println(objects);
+
 
     }
 }
