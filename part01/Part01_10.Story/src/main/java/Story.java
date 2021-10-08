a577897ed1bc8d810f0b7d39074d3468cc35a7be
yavuzzs <vaohdesign@gmail.com>
2021-10-08 16:24:17 +0300

    Story

--- part01/Part01_10.Story/src/main/java/Story.java
+++ part01/Part01_10.Story/src/main/java/Story.java
@@ -7,6 +7,13 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        String answer = scanner.nextLine();
+        System.out.println("What is the main character called?");
+        String answer1 = scanner.nextLine();
+        System.out.println("What is their job?");
+        String answer2 = scanner.nextLine();
 
+        System.out.println("Here is story: " + "Once upon a time there was " +answer1 + " sawho was a " +answer2 );
     }
 }
